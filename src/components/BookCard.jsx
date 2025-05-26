bba363344b4f16e06baed774922318801d1366a5
Saeid Behjat <saeiddeveloper@yahoo.com>
2025-05-26 22:36:57 +0330

    BookCard
    create BookCard components and styled

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
+import { AiFillHeart } from "react-icons/ai";
+
+import Styles from "./BookCard.module.css";
+
 function BookCard({ data: { title, author, image, language, pages } }) {
+  const [like, setLike] = useState(false);
+
+  const likeHandler = () => {
+    setLike((like) => !like);
+  };
   return (
-    <div>
+    <div className={Styles.card}>
       <img src={image} alt={title} />
-      <div>
+      <div className={Styles.info}>
         <h3>{title}</h3>
         <p>{author}</p>
         <div>
-            <span>{language}</span>
-            <span>{pages} Pages</span>
+          <span>{language}</span>
+          <span>{pages} Pages</span>
         </div>
       </div>
+      <button onClick={likeHandler}>
+        <AiFillHeart color={like ? "red" : "#e0e0e0"} fontSize="1.8rem" />
+      </button>
     </div>
   );
 }
